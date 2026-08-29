1429b6bd53a058ea38672a8084a5a0f75a61d026
Руслан <57251484+Ruslik31@users.noreply.github.com>
2026-08-29 00:42:02 +0000

    chore: bump version to 1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.drdisagree.teledrive"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
